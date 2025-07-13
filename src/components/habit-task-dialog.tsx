97a9d131630cdcde01d65fa017c5c5f6d3240cb1
Cristian Morales <cristian.morales.go@gmail.com>
2025-07-13 23:54:37 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <FormItem classNam

--- src/components/habit-task-dialog.tsx
+++ src/components/habit-task-dialog.tsx
@@ -207,7 +207,7 @@ export function HabitTaskDialog({ isOpen, onOpenChange, onSave, habitTask }: Hab
                                 selected={field.value}
                                 onSelect={field.onChange}
                                 disabled={(date) =>
-                                date > new Date() || date < new Date("1900-01-01")
+                                  date < new Date("1900-01-01")
                                 }
                                 initialFocus
                             />
