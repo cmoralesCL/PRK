7a0e92b3348815787fde4e4f9fd6a747ccac9eb8
Cristian Morales <cristian.morales.go@gmail.com>
2025-07-27 22:40:23 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <main class="conta

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,20 +1,14 @@
 
 import * as React from 'react';
-import { AnalyticsDashboard } from '@/components/analytics-dashboard';
-import { getAnalyticsDashboardData } from '@/app/actions';
 import { Header } from '@/components/header';
 
 export const dynamic = 'force-dynamic';
 
 export default async function DashboardPage() {
-  const analyticsData = await getAnalyticsDashboardData();
 
   return (
     <>
         <Header hideDatePicker={true} />
-        <main className="container mx-auto px-4 sm:px-6 lg:px-8 py-8">
-            <AnalyticsDashboard data={analyticsData} />
-        </main>
     </>
   );
 }
